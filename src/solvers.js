2b9d65b6c33e909caaaa5b1e9d835cfe702a33c3
Juan Marin <benjamin.v.stone@gmail.com>
2014-03-05 15:20:57 -0800

    N rooks done

--- src/solvers.js
+++ src/solvers.js
@@ -13,13 +13,47 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+var PieceType = {};
+PieceType['empty'] = 0;
+PieceType['rook'] = 1;
+PieceType['attack'] = 2;
+PieceType['queen'] = 3;
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+window.findNRooksSolution = function(n, board) {
+  board = board || makeEmptyBoard(n);
+  var possibleBoards = placeRook(board);
+  n--;
+
+  if (n === 0){
+    console.log('Single solution for ' + n + ' rooks:', JSON.stringify(possibleBoards));
+    return possibleBoards;
+  } else (n > 0){
+    var arr = [];
+    for (var i = 0; i < possibleBoards.length; i++){
+      arr = arr.concat( findNRooksSolution(n, possibleBoards[i]) );
+    }
+    return arr;
+  }
 };
 
+
+
+window.placeRook = function(board){
+  var possibleBoards = [];
+
+  for (var x = 0; x < board.length; x++){
+    for (var y = 0; y < board.length; y++){
+      if (board[x][y] === PieceType.empty){
+        var b = $.extend(true, [], board);
+        b[x][y] = PieceType.rook;
+        updateBoard(x, y, b);
+        possibleBoards.push(b);
+      }
+    }
+  }
+
+  return possibleBoards;
+}
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
@@ -47,4 +81,25 @@ window.countNQueensSolutions = function(n) {
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
+};
+
+window.updateBoard = function(row, col, board){
+  for (var y = 0; y < board[row].length; y++){
+    if (col !== y){
+      board[row][y] = PieceType.attack;
+    }
+  }
+  for (var x = 0; x < board[col].length; x++){
+    if (row !== x){
+      board[x][col] = PieceType.attack;
+    }
+  }
+}
+
+var makeEmptyBoard = function(n) {
+  return _(_.range(n)).map(function() {
+    return _(_.range(n)).map(function() {
+      return PieceType.empty;
+    });
+  });
 };
