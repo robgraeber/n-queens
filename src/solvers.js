18028c49666ed5ba05004c4c8ba8355d06aeabc1
Juan Marin <benjamin.v.stone@gmail.com>
2014-03-05 19:51:38 -0800

    Solved N Rooks, kinda

--- src/solvers.js
+++ src/solvers.js
@@ -18,31 +18,94 @@ PieceType['empty'] = 0;
 PieceType['rook'] = 1;
 PieceType['attack'] = 2;
 PieceType['queen'] = 3;
+//rotate 90 degrees counter-clockwise
+var rotateBoard = function(board){
+  var n = board.length;
+  var b = $.extend(true, [], board);
+  var tmp;
+  for (var i=0; i<n/2; i++){
+    for (var j=i; j<n-i-1; j++){
+      tmp=b[i][j];
+      b[i][j]=b[j][n-i-1];
+      b[j][n-i-1]=b[n-i-1][n-j-1];
+      b[n-i-1][n-j-1]=b[n-j-1][i];
+      b[n-j-1][i]=tmp;
+    }
+  }
+  return b;
+}
+
+var removeDuplicates = function (collection){
+  var strCollection = _.map(collection, function(item){
+    return JSON.stringify(item);
+  });
+  strCollection = _.uniq(strCollection);
+  return _.map(strCollection, function(item){
+    return JSON.parse(item);
+  });
+}
+
+var removeDuplicateRotations = function(boards){
+  var rotatedBoards = [];
+  for(var i = 0; i < boards.length; i++){
+    var b1 = boards[i];
+    var b2 = rotateBoard(b1);
+    var b3 = rotateBoard(b2);
+    var b4 = rotateBoard(b3);
+    b1 = JSON.stringify(b1);
+    b2 = JSON.stringify(b2);
+    b3 = JSON.stringify(b3);
+    b4 = JSON.stringify(b4);
+    var found = false;
+    for(var j = i+1;  j < boards.length; j++){
+      var a1 = JSON.stringify(boards[j]);
+      if(a1 === b1 || a1 === b2 || a1 === b3 || a1 === b4){
+        found = true;
+        break;
+      }
+    }
+    if(!found){
+      rotatedBoards.push(boards[i]);
+    };
+  }
+  return removeDuplicates(rotatedBoards);
+}
 
 window.findNRooksSolution = function(n, board) {
   board = board || makeEmptyBoard(n);
-  var possibleBoards = placeRook(board);
+  var possibleBoards = placeRook(board, n==1);
   n--;
 
   if (n === 0){
-    console.log('Single solution for ' + n + ' rooks:', JSON.stringify(possibleBoards));
+    // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(possibleBoards));
     return possibleBoards;
-  } else (n > 0){
+  } else if (n > 0){
     var arr = [];
     for (var i = 0; i < possibleBoards.length; i++){
       arr = arr.concat( findNRooksSolution(n, possibleBoards[i]) );
     }
-    return arr;
+    return removeDuplicates(arr);
   }
 };
+window.debugFindNRooksSolution = function(n, board){
+  var before = (new Date()).getTime();
+  var results = findNRooksSolution(n,board);
+  var after = (new Date()).getTime();
+  console.log("Time Taken", after-before);
+  return JSON.stringify(results);
+
+}
 
 
-
-window.placeRook = function(board){
+window.placeRook = function(board, firstTime){
+  firstTime = false;
   var possibleBoards = [];
-
-  for (var x = 0; x < board.length; x++){
-    for (var y = 0; y < board.length; y++){
+  var boardLength = board.length;
+  if(firstTime){
+    boardLength = Math.ceil(board.length/2);
+  }
+  for (var x = 0; x < boardLength; x++){
+    for (var y = 0; y < boardLength; y++){
       if (board[x][y] === PieceType.empty){
         var b = $.extend(true, [], board);
         b[x][y] = PieceType.rook;
